d30b31e6bfa39f071bcffa421509b5fe10b92e18
ilkerulusoy <ilker@ilkerulusoy.com.tr>
2024-12-18 14:36:12 +0300

    docc plugin is deleted.

--- Package.swift
+++ Package.swift
@@ -17,6 +17,9 @@ let package = Package(
             name: "Extensions",
             targets: ["Extensions"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
@@ -24,9 +27,6 @@ let package = Package(
             name: "Extensions",
             swiftSettings: [
                 .enableExperimentalFeature("StrictConcurrency")
-            ],
-            plugins: [
-                .plugin(name: "DocC Plugin", package: "SwiftDocCPlugin")
             ]
         ),
     ]
